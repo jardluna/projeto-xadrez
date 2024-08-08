61df53fc8fe5dcd9ef6f86554922cbaff237a40d
Matheus Luna <matheus-deluna@hotmail.com>
2024-08-08 20:46:40 -0300

    Lógica de xeque

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.net.MulticastSocket;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -56,6 +55,9 @@ public class UI {
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
